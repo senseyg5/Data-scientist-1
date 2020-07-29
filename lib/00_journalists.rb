e0dde1efae9d368d119a7deb8020d3cb2077ec99
lafatra L <lafatra02@GMAIL.COM>
2020-07-30 01:34:21 +0300

    Add files via upload

---
+++ lib/00_journalists.rb
@@ -0,0 +1,137 @@
+
+def liste
+	["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+end
+
+#Demande de nombre des journalistes dans le tableau
+def nombreJournaliste(nbr)
+	puts "\n"
+	puts "1- Combien y a t'il de journalistes dans ce array ?"
+	puts "\n"
+	puts "=====> Il y a #{nbr.count} journalistes dans ce tableau!"
+	puts "\n"
+end
+nbr = liste
+nombreJournaliste(nbr)
+
+#Demande si combien des elements dans le tableau comporte de nombre
+def nombreHandles(nH)
+
+	number_count = 0
+	for numero in nH do
+	    if numero.match(/[0-9]/)
+	      number_count +=1
+	    end
+	end
+	puts "\n"
+	puts "2- Combien d'handle contiennent un numéro ?"
+	puts "\n"
+	puts "=====> Il y en a #{number_count} qui contient de numero"
+	puts "\n"
+end
+#nbr1 = liste
+nombreHandles(nbr)
+
+#Demande si combien des elements dans le tableau comporte de le mot aude
+
+def cnombreAude(nAude)
+  	count = 0
+  	nAude.each do |n|
+	    if n.match(/[a][u][d][e]/)
+	      	count +=1
+	    end
+  	end
+	puts "3- Combien d'handle contiennent les 4 lettres du prénom \"Aude\" à la suite (sans faire attention à la casse) ?"
+	puts "\n"
+	puts "=====> Il y en a #{count} handles qui contient aude"
+	puts "\n"
+end
+
+cnombreAude(nbr)
+
+#Demande si combien des elements dans le tableau commance par un majuscule
+
+def nombreMaj(nM)
+  	nbr = 0
+  	for maj in nM
+      	if maj.match(/@[A-Z]/)
+      		nbr +=1
+      	end
+	end
+	puts "4- Combien commencent par une majuscule ?"
+	puts "\n"
+	puts "=====> Il y a #{nbr} handles qui commance par majuscule!"
+	puts "\n"
+end
+
+nombreMaj(nbr)
+
+#Demande si combien des elements dans le tableau contient de majuscule
+
+def nombreMajTab(nMT)
+  	count = 0
+  	for n in nMT do
+      	if n.match(/[A-Z]/)
+      		count +=1
+      	end
+  	end
+  	puts "5- Combien contiennent une majuscule ?"
+  	puts "\n"
+  	puts "=====> Il y a #{count} handles qui contient de majuscule"
+  	puts "\n"
+end
+
+nombreMajTab(nbr)
+
+#Nombre d'underscore
+
+def nombreUnderscore(nU)
+  	nbr = 0
+  	for u in nU do
+      	if u.match(/[_]/)
+      		nbr +=1
+      	end
+  	end
+  	puts "6- Combien d'underscore dans tous les pseudos confondus ?"
+  	puts "\n"
+  	puts "=====> Il y en a #{nbr} underscore"
+  	puts "\n"
+end
+
+nombreUnderscore(nbr)
+
+#Ordre par alphabetique du tableau
+
+def ordreAlphabetique()
+ 	puts "7- Trie la liste de handle par ordre alphabétique"
+  	puts "\n"
+	return  puts liste.sort
+  	puts"\n"
+end
+ordreAlphabetique
+
+#Les 50 plus courte des handles
+
+def nbr50Court(nbr)
+	puts "\n"
+	nbr = liste.sort_by(&:length)[0..50]
+	puts "8- Quels sont les 50 handles les plus courts de cette array ?"
+	puts "\n"
+	puts "----- Les 50 plus courte des handles sont:"
+	puts "\n"
+	nbr.each do |x|
+		 puts "====> #{x} \n"
+  	end
+  	puts "\n"
+end
+
+nbr50Court(nbr)
+#Position de @epenser dans la tableau
+
+def positionPenser()
+ 	puts "9- Quelle est la position dans l'array de la personne @epenser ?"
+  	puts "=====> @epenser est sur la ligne #{liste.index("@epenser")}"
+  	puts
+end
+
+positionPenser()
